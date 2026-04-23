ba3401c71bb346d0b20ba08fbcaa6c3c3292a061
SanalOwnerTR <yusufalp841@gmail.com>
2026-04-23 21:06:00 +0300

    Update Lua.lua

--- Lua.lua
+++ Lua.lua
@@ -1,48 +1,46 @@
-local status, err = pcall(function()
-local SkinManager = require("client.module_framework.SkinManager")
-    local UI_Mgr = require("client.slua_ui_framework.manager")
+local function ApplySkins()
+    local status, err = pcall(function()
+        local SkinManager = require("client.module_framework.SkinManager")
+        if not SkinManager then return end
 
+        local TargetSkins = {
+            [101001] = 1101001265,
+            [102002] = 1102002424,
+            [101003] = 1101003219,
+            [101004] = 1101004046,
+            [101008] = 1101008163,
+            [101006] = 1101006085
+        }
 
-    local TargetSkins = {
-        [101001] = 1101001265, 
-        [102002] = 1102002424, 
-        [101003] = 1101003219, 
-        [101004] = 1101004046, 
-        [101008] = 1101008163,  
-        [101006] = 1101006085   
-    }
-
-    if SkinManager then
         for weaponID, skinID in pairs(TargetSkins) do
-    
             if SkinManager.AddSkinData then
                 SkinManager.AddSkinData(weaponID, skinID)
             end
-            
-         
             if SkinManager.SetSkinOwned then
                 SkinManager.SetSkinOwned(weaponID, skinID, true)
             end
-
-         
             if SkinManager.SelectSkin then
                 SkinManager.SelectSkin(weaponID, skinID)
             end
         end
-    end
 
-   
-    local LobbyUI = UI_Mgr.GetUI(UI_Mgr.UI_Config.Lobby_Main_UIBP)
-    if LobbyUI then
-        if LobbyUI.RefreshInventory then
-            LobbyUI:RefreshInventory()
+        local UI_Mgr = require("client.slua_ui_framework.manager")
+        if UI_Mgr then
+            local LobbyUI = UI_Mgr.GetUI(UI_Mgr.UI_Config.Lobby_Main_UIBP)
+            if LobbyUI then
+                if LobbyUI.RefreshInventory then
+                    LobbyUI:RefreshInventory()
+                end
+                if LobbyUI.UpdateWeaponDisplay then
+                    LobbyUI:UpdateWeaponDisplay()
+                end
+            end
         end
-        if LobbyUI.UpdateWeaponDisplay then
-            LobbyUI:UpdateWeaponDisplay()
-        end
-    end
-end)
+    end)
 
-if not status then
-   
+    if not status then
+        print("[SkinManager] Error: " .. tostring(err))
+    end
 end
+
+ApplySkins()
